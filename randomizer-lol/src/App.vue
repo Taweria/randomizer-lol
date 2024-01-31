e8d15f47d532526072417d7185b78c6afd62308d
Taweria <elodieali.pro@gmail.com>
2024-01-31 10:19:44 +0100

    Style + fix few images

--- randomizer-lol/src/App.vue
+++ randomizer-lol/src/App.vue
@@ -21,10 +21,46 @@ const getRandomInt = (max) => {
 </script>
 
 <template>
+    <div class="div-button"><button @click="getRandomChamp"> Random </button></div>
     <Card v-if="randomChamp != null" :champion="randomChamp" />
-    <button @click="getRandomChamp"> Random </button>
 </template>
 
-<style scoped>
+<style>
+body{
+    background-color: #252525;
+}
+.div-button{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 25px 0;
 
+}
+button {
+  font-family: 'Marcellus SC', serif;
+  font-size: 30px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  padding: 5px 15px; 
+  background: #1e2328;
+  color: #cdbe91;
+  box-shadow: inset 0 0 2px #000000;
+  border-image: linear-gradient(to bottom, #c8aa6d, #7a5c29);
+  border-image-slice: 1;
+  border-width: 2px;
+}
+
+button:hover {
+  text-shadow: 0 0 5px #ffffff80;
+  box-shadow: 0 0 8px 0 #ffffff50;
+  background: linear-gradient(to bottom, #1e2328, #433d2b);
+  cursor: pointer;
+  transition: 0.1s;
+}
+
+button:active {
+  text-shadow: none;
+  box-shadow: none;
+  color: #cdbe9130;
+}
 </style>
--- randomizer-lol/src/components/Card.vue
+++ randomizer-lol/src/components/Card.vue
@@ -2,21 +2,70 @@
 import { computed } from 'vue';
 
 const props = defineProps(['champion']);
-console.log(props.champion);
 
 const splash = computed(() => {
-    return `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${props.champion.name}_0.jpg`;
+    const cleanedChampionName = props.champion.name.replace(/\s+/g, '').replace(/'/g, '').replace(/\./g, '');
+    return `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${cleanedChampionName}_0.jpg`;
 });
+
 </script>
 
 <template>
-    <pre> {{ champion }}</pre>
+    <!-- <pre> {{ champion }}</pre> -->
     <img :src="splash" alt="champ_img">
     <h2> {{ props.champion.name }}</h2>
     <h3> "{{ props.champion.title }}"</h3>
-    <div v-for="tag in props.champion.tags"> {{ tag }} </div>
+    <div class="div-tags">
+        <div v-for="tag in props.champion.tags" class="tags"> {{ tag }} </div>
+    </div>
 </template>
 
 <style scoped>
+img{
+    display: block;
+    margin: 0 auto;
+    width: 100%;
+    max-width: 800px;
+    border-radius: 10px;
+}
+h2{
+    color: #fff;
+    font-size: 2rem;
+    font-weight: 400;
+    margin: 0 auto;
+    text-align: center;
+    text-transform: uppercase;
+    /* text-shadow: 0 0 10px #e3df67; */
+}
+h3{
+    color: #fff;
+    font-size: 25px;
+    font-weight: 400;
+    margin: 15px 0;
+    text-align: center;
+    text-transform: uppercase;
+    text-decoration: underline;
+    text-underline-offset: 10px;
 
+}
+.div-tags{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 25px 0;
+}
+.tags{
+    font-family: 'Marcellus SC', serif;
+    font-size: 25px;
+    font-weight: bold;
+    letter-spacing: 1px;
+    padding: 5px 15px; 
+    background: #1e2328;
+    color: #cdbe91;
+    box-shadow: inset 0 0 2px #000000;
+    border-image: linear-gradient(to bottom, #c8aa6d, #7a5c29);
+    border-image-slice: 1;
+    border-width: 2px;
+    margin: 0 5px;
+}
 </style>
